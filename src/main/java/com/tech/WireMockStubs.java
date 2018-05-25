92e6e2893d7ed526312b24ebf1a65794778ac9d4
Lokesh <Lokeshg.mca@gmail.com>
2018-05-25 16:03:23 -0400

    Added stubs like Proxying , Faults , Various responses and reset/shutdown

--- src/main/java/com/tech/WireMockStubs.java
+++ src/main/java/com/tech/WireMockStubs.java
@@ -1,5 +1,7 @@
 package com.tech;
 
+import com.github.tomakehurst.wiremock.http.Fault;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED;
@@ -117,5 +119,85 @@ public class WireMockStubs {
 
 
     }
+
+    public void responseFormats() {
+
+        //respond back with status Ok - 200 status cod
+        stubFor(delete("/ok")
+                .willReturn(ok()));
+
+        //respond back status ok with message
+        stubFor(get("/withbody")
+                .willReturn(ok("some body content")));
+
+        //respond with json message , we can send string , byte and number formats
+        stubFor(get("/json")
+                .willReturn(okJson("{ \"message\": \"This is json\" }")));
+
+        //responds with redirected url
+        stubFor(post("/redirect")
+                .willReturn(temporaryRedirect("/json")));
+
+        //responds with unauthorized status
+        stubFor(post("/unauthorized")
+                .willReturn(unauthorized()));
+
+        //responds with status code 500 - here server error
+        stubFor(put("/status")
+                .willReturn(status(500)));
+
+        //responds with status Message
+        stubFor(get("/statusMSG")
+                .willReturn(aResponse()
+                        .withStatusMessage("Define your Status Message")));
+    }
+
+    public void faultHandling() {
+
+        //Send an OK status header, then garbage, then close the connection.
+        stubFor(get(urlEqualTo("/fault1"))
+                .willReturn(aResponse().withFault(Fault.MALFORMED_RESPONSE_CHUNK)));
+
+        //Return a completely empty response
+        stubFor(get(urlEqualTo("/fault2"))
+                .willReturn(aResponse().withFault(Fault.EMPTY_RESPONSE)));
+
+        //Send garbage then close the connection.
+        stubFor(get(urlEqualTo("/fault3"))
+                .willReturn(aResponse().withFault(Fault.RANDOM_DATA_THEN_CLOSE)));
+    }
+
+    public void delayedReponses(){
+
+        //delayed response
+        stubFor(get(urlEqualTo("/delayed")).willReturn(
+                aResponse()
+                        .withStatus(200)
+                        .withBody("This Message is displaying after 2000 milli sec")
+                        .withFixedDelay(2000)));
+
+        //send response as chunks
+        stubFor(get("/chunked/delayed").willReturn(
+                aResponse()
+                        .withBody("Chunked response ")
+                        .withChunkedDribbleDelay(5, 5000)));
+
+
+    }
+
+    public void proxying(){
+
+        //Proxying
+        stubFor(get(urlMatching("/proxy"))
+                .willReturn(aResponse().proxiedFrom("http://www.wiremock.com")));
+
+        // Inject user agent to trigger rendering of mobile version of website
+        stubFor(get(urlMatching("/test/proxy/.*"))
+                .willReturn(aResponse()
+                        .proxiedFrom("http://papajohns.com")
+                        .withAdditionalRequestHeader("User-Agent", "Mozilla/5.0 (iPhone; U; CPU iPhone)")));
+
+    }
+
 
 }
